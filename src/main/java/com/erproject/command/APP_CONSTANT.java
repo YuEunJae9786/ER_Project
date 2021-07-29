51e3a9b56e7d0adca24a632163942fddf9ca64c3
YuEunJae <rsorry9786@gmail.com>
2021-07-29 21:20:59 +0900

    git ignore cached test

--- src/main/java/com/erproject/command/APP_CONSTANT.java
+++
@@ -1,7 +0,0 @@
-package com.erproject.command;
-
-public class APP_CONSTANT {
-	
-	public static final String UPLOAD_PATH = "C:\\Users\\user\\Desktop\\Project\\ER_Project\\src\\main\\webapp\\resources\\img";
-
-}
